619ee52a8baa256606b768ccdc79764ac0b3d8d4
AlexWSN <sasualexandra97@gmail.com>
2025-06-07 19:36:42 +0300

    commit

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let index = 0;
+
+function updateSlider() {
+  const width = images[0].clientWidth;
+  slides.style.transform = `translateX(${-index * width}px)`;
+}
+
+next.addEventListener('click', () => {
+  index = (index + 1) % images.length;
+  updateSlider();
+});
+
+prev.addEventListener('click', () => {
+  index = (index - 1 + images.length) % images.length;
+  updateSlider();
+});
+
+window.addEventListener('resize', updateSlider);
+window.addEventListener('load', updateSlider);
